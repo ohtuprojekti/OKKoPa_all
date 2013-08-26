d89bcee90345b82a37f0503e17bdb758f8309f87
Hanna Hirvonen <hanna.hirvonen@cs.helsinki.fi>
2013-08-26 15:51:20 +0300

    storytestejä lisätty

--- OKKoPa_GUI/src/test/java/easyb/AddStory.groovy
+++ OKKoPa_GUI/src/test/java/easyb/AddStory.groovy
@@ -1,34 +1,93 @@
-package stories
+package easyb;
 
 import org.openqa.selenium.*
 import org.openqa.selenium.htmlunit.*;
 import fi.helsinki.cs.okkopa.*
-import fi.helsinki.cs.okkopa.database.OkkopaDatabase
-import com.thoughtworks.selenium.Selenium
-import com.thoughtworks.selenium.DefaultSelenium
+import fi.helsinki.cs.okkopa.database.OkkopaDatabase;
+import com.thoughtworks.selenium.Selenium;
+import com.thoughtworks.selenium.DefaultSelenium;
 
-description "Adding references"
+description "Adding anonymous references"
  
-scenario "User can give right reference with right username", {
+scenario "User gives empty reference code and username", {
     
     Selenium selenium
     
-    given "Proper url given",{
+    given "Viitteiden rekisteröinti is selected",{
         WebDriver driver = new HtmlUnitDriver(true);
-        String baseUrl = "http://localhost:8080/";
+        String baseUrl = "http://localhost:8081/";
         selenium = new WebDriverBackedSelenium(driver, baseUrl);
-    }
- 
-    when "moo", {
         selenium.open("/");
         selenium.click("link=Viitteiden rekisteröinti");
         selenium.waitForPageToLoad("30000");
+    }
+ 
+    when "empty form is submitted", {       
         selenium.click("name=Lähetä");
         selenium.waitForPageToLoad("30000");
     }
  
-    then "user will be logged in to system", {
+    then "user will see an error message", {
         selenium.getText("//p[3]").shouldBe "- Käyttäjätunnus pitää olla yli kaksi merkkiä pitkä."
         selenium.stop();
     }
 }
+
+scenario "User gives incorrect reference code with correct username", {
+    
+    Selenium selenium
+    
+    given "Viitteiden rekisteröinti is selected",{
+        WebDriver driver = new HtmlUnitDriver(true);
+        String baseUrl = "http://localhost:8081/";
+        selenium = new WebDriverBackedSelenium(driver, baseUrl);
+        selenium.open("/");
+        selenium.click("link=Viitteiden rekisteröinti");
+        selenium.waitForPageToLoad("30000");
+    }
+ 
+    when "incorrect reference code is submitted", { 
+        selenium.typeKeys("name=id", "testi");
+	selenium.typeKeys("name=code", "shgsh57");
+        selenium.click("name=Lähetä");
+        selenium.waitForPageToLoad("30000");
+    }
+ 
+    then "user will see an error message", {
+        selenium.getText("//p[3]").shouldBe "- Kirjoitit viitteesi väärin, tarkista oikeinkirjoitus."
+        selenium.stop();
+    }
+}
+//
+//scenario "User gives correct reference code and username", {
+//    
+//    Selenium selenium
+//    
+//    given "Viitteiden rekisteröinti is selected",{
+//        WebDriver driver = new HtmlUnitDriver(true);
+//        String baseUrl = "http://localhost:8081/";
+//        selenium = new WebDriverBackedSelenium(driver, baseUrl);
+//        selenium.open("/");
+//        selenium.click("link=Viitteiden rekisteröinti");
+//        selenium.waitForPageToLoad("30000");
+//    }
+// 
+//    when "correct reference code is submitted", { 
+//        
+//        newReference = new fi.helsinki.cs.okkopa.reference.Reference(6);
+//        stringReference = newReference.getReference();
+//        
+//        db = new OkkopaDatabase();
+//        db.addQRCode(stringReference); 
+//        
+//        selenium.typeKeys("name=id", "testi");
+//	selenium.typeKeys("name=code", ""+stringReference);
+//        selenium.click("name=Lähetä");
+//        selenium.waitForPageToLoad("30000");
+//    }
+// 
+//    then "reference code is registered", {
+//        selenium.getText("//p[3]").shouldBe "- Homma OK! Koe tulee sinulle kunhan se on tarkistettu."
+//        selenium.stop();
+//    }
+//}
