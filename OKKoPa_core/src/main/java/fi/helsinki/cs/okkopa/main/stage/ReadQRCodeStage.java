a617aecd45bc5649ade12f48bbdec28dd2d42cc6
TeeAaTeeUu <tatu.tallberg@gmail.com>
2013-08-27 13:29:26 +0300

    cleant methods v9

--- OKKoPa_core/src/main/java/fi/helsinki/cs/okkopa/main/stage/ReadQRCodeStage.java
+++ OKKoPa_core/src/main/java/fi/helsinki/cs/okkopa/main/stage/ReadQRCodeStage.java
@@ -34,32 +34,45 @@ public class ReadQRCodeStage extends Stage<ExamPaper, ExamPaper> {
         this.exceptionLogger = exceptionLogger;
         this.fileSaver = fileSaver;
         this.pdfProcessor = pdfProcessor;
-        saveErrorFolder = settings.getProperty("exampaper.saveunreadablefolder");
-        saveOnExamPaperPDFError = Boolean.parseBoolean(settings.getProperty("exampaper.saveunreadable"));
+        
+        this.saveErrorFolder = settings.getProperty("exampaper.saveunreadablefolder");
+        this.saveOnExamPaperPDFError = Boolean.parseBoolean(settings.getProperty("exampaper.saveunreadable"));
     }
 
     @Override
     public void process(ExamPaper examPaper) {
         try {
-            examPaper.setPageImages(pdfProcessor.getPageImages(examPaper));
-            examPaper.setQRCodeString(pdfProcessor.readQRCode(examPaper));
+            readAndSetQRCode(examPaper);
+            
         } catch (PdfException | NotFoundException ex) {
             exceptionLogger.logException(ex);
             LOGGER.debug("QR-koodia ei pystytty lukemaan.");
+            
             batch.addFailedScan();
+            
             if (saveOnExamPaperPDFError) {
-                try {
-                    InputStream stream = new ByteArrayInputStream(examPaper.getPdf());
-                    fileSaver.saveInputStream(stream, saveErrorFolder, "" + System.currentTimeMillis() + ".pdf");
-                    IOUtils.closeQuietly(stream);
-                    LOGGER.debug("Tallennettin virheellinen PDF.");
-                } catch (FileAlreadyExistsException ex1) {
-                    exceptionLogger.logException(ex);
-                }
+                saveErrorExamPaper(examPaper, ex);
             }
             // Don't continue if we couldn't read QR code.
             return;
         }
         processNextStages(examPaper);
+    }
+
+    private void saveErrorExamPaper(ExamPaper examPaper, final java.lang.Exception ex) {
+        try {
+            InputStream stream = new ByteArrayInputStream(examPaper.getPdf());
+            fileSaver.saveInputStream(stream, saveErrorFolder, "" + System.currentTimeMillis() + ".pdf");
+            IOUtils.closeQuietly(stream);
+            
+            LOGGER.debug("Tallennettin virheellinen PDF.");
+        } catch (FileAlreadyExistsException ex1) {
+            exceptionLogger.logException(ex);
+        }
+    }
+
+    private void readAndSetQRCode(ExamPaper examPaper) throws NotFoundException, PdfException {
+        examPaper.setPageImages(pdfProcessor.getPageImages(examPaper));
+        examPaper.setQRCodeString(pdfProcessor.readQRCode(examPaper));
     }
 }
