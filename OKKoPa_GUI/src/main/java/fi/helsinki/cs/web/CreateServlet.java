c3c33e7772378b25852dbeffc67ce6db0df277bc
TeeAaTeeUu <tatu.tallberg@gmail.com>
2013-08-27 15:30:47 +0300

    more settings for gui

--- OKKoPa_GUI/src/main/java/fi/helsinki/cs/web/CreateServlet.java
+++ OKKoPa_GUI/src/main/java/fi/helsinki/cs/web/CreateServlet.java
@@ -25,6 +25,20 @@ public class CreateServlet extends HttpServlet {
         request.setAttribute("message", Settings.instance.getProperty("gui.create.header"));
         request.setAttribute("help", Settings.instance.getProperty("gui.create.help"));
         
+        request.setAttribute("amount", Settings.instance.getProperty("gui.create.text.amount"));
+        request.setAttribute("size", Settings.instance.getProperty("gui.create.text.size"));
+        
+        request.setAttribute("letters", Settings.instance.getProperty("gui.create.text.letters"));
+        request.setAttribute("lettersYes", Settings.instance.getProperty("gui.create.text.letters.yes"));
+        request.setAttribute("lettersNo", Settings.instance.getProperty("gui.create.text.letters.no"));
+        
+        System.out.println("\n möö\n\n" + Settings.instance.getProperty("gui.create.text.back") + "\n möö\n\n");
+        
+        request.setAttribute("back", Settings.instance.getProperty("gui.create.text.back"));
+        request.setAttribute("backTxt", Settings.instance.getProperty("gui.create.text.back.txt"));
+        request.setAttribute("backZip", Settings.instance.getProperty("gui.create.text.back.zip"));
+        
+        
         RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/create.jsp");
         dispatcher.forward(request, response);
     }
