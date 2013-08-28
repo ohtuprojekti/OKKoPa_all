12a4525010ff93e0a4108992d5b19b35edf1562c
Hanna Hirvonen <hanna.hirvonen@cs.helsinki.fi>
2013-08-28 10:30:41 +0300

    pomia muokattu

--- OKKoPa_GUI/src/test/java/easyb/AddStory.groovy
+++ OKKoPa_GUI/src/test/java/easyb/AddStory.groovy
@@ -1,67 +1,60 @@
-//package easyb;
-//
-//import org.openqa.selenium.*
-//import org.openqa.selenium.htmlunit.*;
-//import fi.helsinki.cs.okkopa.*
-//import fi.helsinki.cs.okkopa.database.OkkopaDatabase;
-//import com.thoughtworks.selenium.Selenium;
-//import com.thoughtworks.selenium.DefaultSelenium;
-//import org.openqa.selenium.server.SeleniumServer
-//
-//description "Adding anonymous references"
-// 
-//scenario "User gives empty reference code and username", {
-//    
-//    Selenium selenium
-//    
-//    given "Viitteiden rekisteröinti is selected",{     
-//        server = new SeleniumServer();
-//        server.start();
-//        WebDriver driver = new HtmlUnitDriver(true);
-//        String baseUrl = "http://localhost:4444/wd/hub";
-//        selenium = new WebDriverBackedSelenium(driver, baseUrl);
-//        selenium.open("/add");
-//    }
-// 
-//    when "empty form is submitted", {  
-//        selenium.wait(100000000000);
-//        selenium.click("name=Lähetä");
-//        selenium.waitForPageToLoad("30000");
-//    }
-// 
-//    then "user will see an error message", {
-//        selenium.getText("//p[3]").shouldBe "- Käyttäjätunnus pitää olla yli kaksi merkkiä pitkä. / Username must be at least two characters."
-//        selenium.stop();
-//        server.stop();
-//    }
-//}
+package easyb;
 
-//scenario "User gives incorrect reference code with correct username", {
-//    
-//    Selenium selenium
-//    
-//    given "Viitteiden rekisteröinti is selected",{
-//        server = new SeleniumServer();
-//	server.start();
-//        WebDriver driver = new HtmlUnitDriver(true);
-//        String baseUrl = "http://localhost:8081";
-//        selenium = new WebDriverBackedSelenium(driver, baseUrl);
-//        selenium.open("/add");
-//    }
-// 
-//    when "incorrect reference code is submitted", { 
-//        selenium.typeKeys("name=id", "testi");
-//	selenium.typeKeys("name=code", "shgsh57");
-//        selenium.click("name=Lähetä");
-//        selenium.waitForPageToLoad("30000");
-//    }
-// 
-//    then "user will see an error message", {
-//        selenium.getText("//p[3]").shouldBe "- Kirjoitit viitteesi väärin, tarkista oikeinkirjoitus. / Reference is incorrect. Pleace check it and try again."
-//        selenium.stop();
-//        server.stop();
-//    }
-//}
+import org.openqa.selenium.*
+import org.openqa.selenium.htmlunit.*;
+import fi.helsinki.cs.okkopa.*
+import fi.helsinki.cs.okkopa.database.OkkopaDatabase;
+import com.thoughtworks.selenium.Selenium;
+import com.thoughtworks.selenium.DefaultSelenium;
+import org.openqa.selenium.server.SeleniumServer
+
+description "Adding anonymous references"
+ 
+scenario "User gives empty reference code and username", {
+   
+    Selenium selenium
+    
+    given "Viitteiden rekisteröinti is selected",{     
+        WebDriver driver = new HtmlUnitDriver(true);
+        String baseUrl = "http://localhost:8081";
+        selenium = new WebDriverBackedSelenium(driver, baseUrl);
+        selenium.open("/add");
+    }
+ 
+    when "empty form is submitted", {  
+        selenium.click("name=Lähetä");
+        selenium.waitForPageToLoad("30000");
+    }
+ 
+    then "user will see an error message", {
+        selenium.getText("//p[3]").shouldBe "- Käyttäjätunnus pitää olla yli kaksi merkkiä pitkä./ Username must be at least two characters."
+        selenium.stop();
+    }
+}
+
+scenario "User gives incorrect reference code with correct username", {
+    
+    Selenium selenium
+    
+    given "Viitteiden rekisteröinti is selected",{
+        WebDriver driver = new HtmlUnitDriver(true);
+        String baseUrl = "http://localhost:8081";
+        selenium = new WebDriverBackedSelenium(driver, baseUrl);
+        selenium.open("/add");
+    }
+ 
+    when "incorrect reference code is submitted", { 
+        selenium.typeKeys("name=id", "testi");
+	selenium.typeKeys("name=code", "shgsh57");
+        selenium.click("name=Lähetä");
+        selenium.waitForPageToLoad("30000");
+    }
+ 
+    then "user will see an error message", {
+        selenium.getText("//p[3]").shouldBe "- Kirjoitit viitteesi väärin, tarkista oikeinkirjoitus./ Reference is incorrect. Pleace check it and try again."
+        selenium.stop();
+    }
+}
 
 
 //
