7b360766c9e40121f9804eade8f9a059f633c4fa
Hanna Hirvonen <hanna.hirvonen@cs.helsinki.fi>
2013-08-28 13:47:07 +0300

    luokka poistettu

--- OKKoPa_core/src/main/java/fi/helsinki/cs/okkopa/main/stage/SendEmailStage.java
+++ OKKoPa_core/src/main/java/fi/helsinki/cs/okkopa/main/stage/SendEmailStage.java
@@ -18,9 +18,6 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-/**
- *
- */
 @Component
 public class SendEmailStage extends Stage<ExamPaper, ExamPaper> {
 
